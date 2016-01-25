f64bdedd262b5eb22cd7f209a21435f5c38ea334
rcastill <rodolfocastillomateluna@gmail.com>
2016-01-24 22:02:58 -0300

    Reestructure some code, now Objects have children. Add RAII idiom for SDL systems

--- GraphicsEngine.cc
+++ GraphicsEngine.cc
@@ -1,6 +1,6 @@
 #include "GraphicsEngine.h"
 
-#include "Texture.h"
+#include <iostream>
 
 GraphicsEngine::GraphicsEngine(string title, int width, int height) :
     windowTitle(title),
@@ -8,9 +8,6 @@ GraphicsEngine::GraphicsEngine(string title, int width, int height) :
     windowHeight(height),
     running(true)
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        throw runtime_error(IMG_GetError());
-
     SDL_Window *tmpWindow = SDL_CreateWindow(
             title.c_str(),
             SDL_WINDOWPOS_UNDEFINED,
@@ -22,7 +19,9 @@ GraphicsEngine::GraphicsEngine(string title, int width, int height) :
     if (tmpWindow == 0)
         throw runtime_error(SDL_GetError());
 
-    sdlWindow.get_deleter() = [](SDL_Window *w) { SDL_DestroyWindow(w); };
+    sdlWindow.get_deleter() = [](SDL_Window *w) {
+        SDL_DestroyWindow(w);
+    };
     sdlWindow.reset(tmpWindow);
 
     SDL_Renderer *tmpRenderer = SDL_CreateRenderer(sdlWindow.get(), -1, SDL_RENDERER_ACCELERATED);
@@ -30,13 +29,18 @@ GraphicsEngine::GraphicsEngine(string title, int width, int height) :
     if (tmpRenderer == 0)
         throw runtime_error(SDL_GetError());
 
-    sdlRenderer.get_deleter() = [](SDL_Renderer *r) { SDL_DestroyRenderer(r); };
+    sdlRenderer.get_deleter() = [](SDL_Renderer *r) {
+        SDL_DestroyRenderer(r);
+    };
     sdlRenderer.reset(tmpRenderer);
 }
 
 GraphicsEngine::~GraphicsEngine()
 {
-    SDL_Quit();
+    for (auto &object : gfxObjects)
+        object->unsetGraphicsEngine();
+
+    cout << "~GraphicsEngine" << endl;
 }
 
 void GraphicsEngine::openWindow()
@@ -78,25 +82,58 @@ void GraphicsEngine::setLoopDelay(int loopDelay)
 
 void GraphicsEngine::mainloop()
 {
-    for (int i = 0; i < gfxObjects.size(); i++) {
-        gfxObjects[i]->setGraphicsEngine(this);
-        gfxObjects[i]->init();
-    }
 
     SDL_Event event;
+    ObjectQueue objectQueue;
+
+    /* Loads queue with initial objects (Root children) */
+    auto loadQueue = [&objectQueue, this] () {
+        for (auto obj : gfxObjects)
+            objectQueue.push(obj);
+    };
+
+
+    /* Iterates over tree in level order and applies each node with callback if node is
+     * enabled */
+    auto doLevel = [&objectQueue] (function<void(shared_ptr<GraphicObject>&)> callback) {
+        while (!objectQueue.empty()) {
+            auto object = objectQueue.front();
+            objectQueue.pop();
+
+            if (!object->enabled())
+                continue;
+
+            callback(object);
+
+            for (auto &child : object->children)
+                objectQueue.push(child);
+        }
+
+    };
+
+    /* Init objects */
+    loadQueue();
+    doLevel([this](shared_ptr<GraphicObject> &obj) {
+        obj->gfx = this;
+        obj->init();
+    });
 
     while (running) {
         SDL_RenderClear(sdlRenderer.get());
 
         while (SDL_PollEvent(&event) != 0) {
-            for (auto object : gfxObjects) {
-                object->update(event);
-            }
+            /* Update objects */
+            loadQueue();
+            doLevel([&event](shared_ptr<GraphicObject> &obj) {
+                obj->update(event);
+            });
         }
 
-        for (auto object : gfxObjects) {
-            object->render();
-        }
+        /* Render objects */
+        loadQueue();
+        doLevel([](shared_ptr<GraphicObject> &obj) {
+            obj->render();
+        });
 
         SDL_RenderPresent(sdlRenderer.get());
         SDL_Delay((Uint32) loopDelay);
@@ -107,27 +144,19 @@ void GraphicsEngine::disposeResource(Resource *resource)
 {
     if (--(resource->refCount) == 0) {
         availableResIds.push_back(resource->id());
+        cout << 1 << endl;
         resources[resource->id()] = 0;
     }
 }
 
 Texture *GraphicsEngine::loadTexture(string path)
 {
-    for (auto &resource : resources) {
-        if (resource && resource->getPath() == path) {
-            if (resource->unique())
-                break;
+    Resource *rawResource = checkResource(path);
+    Texture *foundTexture = dynamic_cast<Texture*>(rawResource);
 
-            Resource *rawResource = resource.get();
-            Texture *texture = dynamic_cast<Texture*>(rawResource);
-
-            if (texture != NULL) {
-                resource->retain();
-                return texture;
-            }
-
-            break;
-        }
+    if (foundTexture != NULL) {
+        foundTexture->retain();
+        return foundTexture;
     }
 
     SDL_Texture *sdlTexture = 0;
@@ -144,24 +173,73 @@ Texture *GraphicsEngine::loadTexture(string path)
     SDL_FreeSurface(loadedSurface);
 
     auto texture = makeTexture(path, sdlTexture);
+    cacheResource(texture);
 
-    ResourceVector::size_type id;
-    if (!availableResIds.empty()) {
-        id = availableResIds.back();
-        availableResIds.pop_back();
-        texture->id(id);
-        resources[id] = texture;
+    return texture.get();
+}
 
-    } else {
-        id = resources.size();
-        resources.push_back(texture);
-    }
+Font *GraphicsEngine::loadFont(string path, int size)
+{
+    Resource *rawResource = checkResource(path);
+    Font *foundFont = dynamic_cast<Font*>(rawResource);
+
+    if (foundFont != NULL)
+        return foundFont;
+
+    auto font = shared_ptr<Font>(new Font(path, size));
+
+    if (!(font->ttfFont))
+        return 0;
+
+    cacheResource(font);
+
+    return font.get();
+}
+
+Texture *GraphicsEngine::makeText(Font *font, string text, Color color, FontQuality quality, bool unique)
+{
+    string path = "text://" + text;
+
+    Resource *rawResource = checkResource(path);
+    Texture *foundTexture = dynamic_cast<Texture*>(rawResource);
+
+    if (foundTexture)
+        return foundTexture;
+
+    SDL_Surface *surface = NULL;
+
+    if (quality == FQ_LOW)
+        surface = TTF_RenderText_Solid(font->ttfFont.get(), text.c_str(), color.getSdlColor());
+    else if (quality == FQ_MID)
+        surface = TTF_RenderText_Shaded(font->ttfFont.get(), text.c_str(), color.getSdlColor(),
+                                        color.negative().getSdlColor());
+    else if (quality == FQ_HIGH)
+        surface = TTF_RenderText_Blended(font->ttfFont.get(), text.c_str(), color.getSdlColor());
+    else
+        throw runtime_error("Invalid font quality");
+
+    if (surface == NULL)
+        return 0;
+
+    SDL_Texture *sdlTexture = SDL_CreateTextureFromSurface(sdlRenderer.get(), surface);
+
+    if (sdlTexture == NULL)
+        return 0;
+
+    SDL_FreeSurface(surface);
+
+    auto texture = makeTexture(path, sdlTexture);
+    texture->unique(unique);
+    cacheResource(texture);
 
     return texture.get();
 }
 
 void GraphicsEngine::renderTexture(Texture *texture, SDL_Rect dstrect)
 {
+    if (!texture)
+        throw runtime_error("Could not render texture.");
+
     SDL_RenderCopy(sdlRenderer.get(),
                    texture->sdlTexture.get(),
                    (texture->fullSource) ? 0 : &(texture->srcrect),
@@ -176,6 +254,9 @@ void GraphicsEngine::renderTexture(Texture *texture, int x, int y, int w, int h)
 
 void GraphicsEngine::renderTexture(Texture *texture, int x, int y)
 {
+    if (!texture)
+        throw runtime_error("Could not render texture.");
+
     int w, h;
     texture->getSize(w, h);
     renderTexture(texture, x, y, w, h);
@@ -185,6 +266,33 @@ void GraphicsEngine::size(int &w, int &h) const
 {
     w = windowWidth;
     h = windowHeight;
+}
+
+Resource *GraphicsEngine::checkResource(string path)
+{
+    for (auto &resource : resources) {
+        if (resource && resource->getPath() == path) {
+            if (resource->unique())
+                return NULL;
+
+            return resource.get();
+        }
+    }
+
+    return NULL;
+}
+
+void GraphicsEngine::cacheResource(shared_ptr<Resource> resource)
+{
+    if (!availableResIds.empty()) {
+        resource->id(availableResIds.back());
+        resources[availableResIds.back()] = resource;
+        availableResIds.pop_back();
+
+    } else {
+        resource->id(resources.size());
+        resources.push_back(resource);
+    }
 }
 
 shared_ptr<Texture> GraphicsEngine::makeTexture(string path, SDL_Texture *raw)
@@ -198,4 +306,20 @@ shared_ptr<Texture> GraphicsEngine::makeTexture(string path, SDL_Texture *raw)
 void GraphicsEngine::stop()
 {
     running = false;
+}
+
+GraphicsEngine::SDL2Systems::SDL2Systems()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        throw runtime_error(IMG_GetError());
+
+    if (TTF_Init() < 0)
+        throw runtime_error(TTF_GetError());
+}
+
+GraphicsEngine::SDL2Systems::~SDL2Systems()
+{
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
 }
--- GraphicsEngine.h
+++ GraphicsEngine.h
@@ -3,13 +3,19 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <string>
 #include <stdexcept>
+#include <queue>
 #include <vector>
 #include <memory>
 #include <functional>
 #include "Color.h"
 #include "GraphicObject.h"
+#include "Texture.h"
+#include "Font.h"
+
+/* TODO: Fix font caching (different sizes) */
 
 using namespace std;
 
@@ -17,11 +23,25 @@ class Resource;
 class Texture;
 
 typedef vector<shared_ptr<GraphicObject>> GfxObjectVector;
+typedef queue<shared_ptr<GraphicObject>> ObjectQueue;
 typedef vector<shared_ptr<Resource>> ResourceVector;
+
+enum FontQuality
+{
+    FQ_HIGH,
+    FQ_MID,
+    FQ_LOW
+};
 
 class GraphicsEngine
 {
 public:
+    struct SDL2Systems
+    {
+        SDL2Systems();
+        ~SDL2Systems();
+    };
+
     GraphicsEngine(string title, int width, int height);
     ~GraphicsEngine();
 
@@ -38,6 +58,9 @@ public:
     void size(int &w, int &h) const;
 
     Texture *loadTexture(string path);
+    Font *loadFont(string path, int size);
+    Texture *makeText(Font *font, string text, Color color, FontQuality quality = FQ_LOW, bool unique = true);
+
     void renderTexture(Texture *texture, SDL_Rect dstrect);
     void renderTexture(Texture *texture, int x, int y, int w, int h);
     void renderTexture(Texture *texture, int x, int y);
@@ -48,6 +71,8 @@ public:
     void stop();
 
 private:
+    Resource *checkResource(string path);
+    void cacheResource(shared_ptr<Resource> resource);
     shared_ptr<Texture> makeTexture(string path, SDL_Texture *raw);
 
     unique_ptr<SDL_Window, function<void(SDL_Window*)>> sdlWindow;
