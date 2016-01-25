70f443a02ffab4f8e371691a00062c863695c0e4
rcastill <rodolfocastillomateluna@gmail.com>
2016-01-24 22:01:39 -0300

    Add enabled semantincs, children and some shortcuts

--- GraphicObject.cc
+++ GraphicObject.cc
@@ -3,27 +3,75 @@
 //
 
 #include "GraphicObject.h"
+#include "GraphicsEngine.h"
+
+#include <iostream>
+
+GraphicObject::GraphicObject() :
+        gfx(nullptr),
+        objectEnabled(true)
+{
+    //cout << "GraphicObject()" << endl;
+}
 
 GraphicObject::~GraphicObject()
 {
-}
+    cout << "~GraphicObject(";
+    if (checkEngine()) {
+        cout << "...";
+        for (Resource *resource : resources)
+            resource->dispose();
 
-void GraphicObject::setGraphicsEngine(GraphicsEngine *gfx)
-{
-    this->gfx = gfx;
-}
+        cout << "dispose);" << endl;
+        resources.clear();
 
+    } else {
+        cout << ");" << endl;
+    }
+}
 
 void GraphicObject::init()
 {
-
 }
 
 void GraphicObject::update(SDL_Event &event)
 {
-
 }
 
 void GraphicObject::render()
 {
+}
+
+
+bool GraphicObject::enabled() const
+{
+    return objectEnabled;
+}
+
+void GraphicObject::enabled(bool objectEnabled)
+{
+    this->objectEnabled = objectEnabled;
+}
+
+Texture *GraphicObject::loadTexture(string path)
+{
+    if (!checkEngine())
+        return nullptr;
+
+    Texture *texture = gfx->loadTexture(path);
+    if (texture) resources.push_back(texture);
+    return texture;
+}
+
+bool GraphicObject::checkEngine() const
+{
+    return gfx != nullptr;
+}
+
+void GraphicObject::unsetGraphicsEngine()
+{
+    gfx = nullptr;
+
+    for (auto& child : children)
+        child->unsetGraphicsEngine();
 }
--- GraphicObject.h
+++ GraphicObject.h
@@ -6,22 +6,50 @@
 #define PROJECT_STRAT_GRAPHICOBJECT_H
 
 #include <SDL2/SDL_events.h>
+#include <vector>
+#include <memory>
+#include "Texture.h"
 
 class GraphicsEngine;
+class GraphicObject;
+
+using namespace std;
+
+
+typedef vector<shared_ptr<GraphicObject>> GfxObjectVector;
+typedef vector<Resource*> RawResourceVector;
 
 class GraphicObject
 {
 public:
+    GraphicObject();
     virtual ~GraphicObject();
 
-    void setGraphicsEngine(GraphicsEngine *gfx);
+    bool enabled() const;
+    void enabled(bool objectEnabled);
 
     virtual void init();
     virtual void update(SDL_Event &event);
     virtual void render();
 
+    template <typename T, typename... A>
+    T *addChild(A... args);
+
+    Texture *loadTexture(string path);
+
 protected:
+    GfxObjectVector children;
     GraphicsEngine *gfx;
+    bool objectEnabled;
+
+private:
+    friend class GraphicsEngine;
+    RawResourceVector resources;
+
+    void unsetGraphicsEngine();
+    bool checkEngine() const;
 };
+
+#include "GraphicObject.tcc"
 
 #endif //PROJECT_STRAT_GRAPHICOBJECT_H
